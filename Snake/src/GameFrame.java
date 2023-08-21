7d3d4f29ce8ffaaed00777a1bcb4e843a8a8178b
Seif <seifmagedmohamed@gmail.com>
2023-08-21 20:23:26 +0300

    Added ImageIcon instead of default java icon

--- Snake/src/GameFrame.java
+++ Snake/src/GameFrame.java
@@ -1,16 +1,20 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
 
 	GameFrame(){
 		
+		
 		this.add(new GamePanel());
 		this.setTitle("Snake");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
+		ImageIcon image = new ImageIcon("snake.png"); // create ImageIcon
+		this.setIconImage(image.getImage()); // change frame Icon
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // changes x to exit out of application instead of hide
+		this.setResizable(false); 
 		this.pack();
 		this.setVisible(true);
-		this.setLocationRelativeTo(null);
+		this.setLocationRelativeTo(null); // centers the JFrame to the screen
 		
 	}
 }
